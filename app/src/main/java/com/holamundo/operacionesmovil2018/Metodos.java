80b5b60fb6ece8c88b667cc281c8dc903e81c641
Roberto <roberto@morales.com.co>
2018-02-26 20:23:16 -0500

    Funacionalidad sumar terminada sin validaciones

---
+++ app/src/main/java/com/holamundo/operacionesmovil2018/Metodos.java
@@ -0,0 +1,13 @@
+package com.holamundo.operacionesmovil2018;
+
+/**
+ * Created by android on 26/02/2018.
+ */
+
+public class Metodos {
+
+    public static double sumar(double n1, double n2){
+        return n1+n2;
+    }
+
+}
